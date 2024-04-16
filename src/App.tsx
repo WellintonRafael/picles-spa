8214717eea6bb52c81f0c2c60d4702855672af27
WellintonRafael <wellintonrafaelsantos@gmail.com>
2024-04-15 22:30:12 -0300

    iniciando estilização de botões

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
+import { Button } from './components/common/Button'
+import { ButtonVariant } from './components/common/Button/Button.constants'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
-    </button>
+    <>
+      <Button variant={ButtonVariant.Default} onClick={() => setCount(count + 1)}>Quero Adotar</Button>
+      <Button variant={ButtonVariant.Disabled} onClick={() => setCount(count + 1)}>Disabled</Button>
+      <Button variant={ButtonVariant.Outlined} onClick={() => setCount(count + 1)}>Outlined</Button>
+      <Button variant={ButtonVariant.Text} onClick={() => setCount(count + 1)}>Text</Button>
+      {count}
+    </>
   )
 }
