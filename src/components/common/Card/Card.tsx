a02632dca49eb58d93f972187f031cc5f64789d0
WellintonRafael <wellintonrafaelsantos@gmail.com>
2024-04-17 22:22:18 -0300

    iniciando axios e skeleton

---
+++ src/components/common/Card/Card.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import { ImageBase64 } from '../ImageBase64/ImageBase64'
+import styles from './Card.module.css'
+
+interface ICard {
+    href: string
+    text: string
+    thumb: string
+}
+
+export function Card({ href, text, thumb }: ICard) {
+    return (
+        <Link to={href} className={styles.card}>
+            <ImageBase64 src={thumb}/>
+            <span>{text}</span>
+        </Link>
+    )
+}
--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,14 +1,24 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import 'react-loading-skeleton/dist/skeleton.css';
+import { Card } from "../../components/common/Card/Card";
 import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid";
-
-
+import styles from './Pets.module.css';
+import { Skeleton } from "../../components/common/Skeleton";
 
 export function Pets() {
+    const [pets, setPets] = useState([]);
+    const [isLoading, setIsLoading] = useState(true)
+
     return (
         <Grid>
-            <Header />
-            <Link to='/pets/id'>Ir para listagem</Link>
+            <div className={styles.container}>
+                <Header />
+                <main className={styles.list}>
+                    <Skeleton count={5} containerClassName={styles.skeleton} />
+                    <Card href="pets/1" text="nina" thumb="" />
+                </main>
+            </div>
         </Grid>
     )
 }
