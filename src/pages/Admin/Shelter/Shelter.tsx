f4ccc46a22d490c47ef4c96b8260dea889bff96a
WellintonRafael <wellintonrafaelsantos@gmail.com>
2024-04-23 22:17:39 -0300

    criação do forWardInput

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,5 +1,16 @@
+import styles from './Shelter.module.css'
+import { Button } from "../../../components/common/Button";
+import { Input } from "../../../components/common/Input";
 import { Panel } from "../../../components/layout/Panel";
 
 export function Shelter() {
-    return <Panel>Formulário de abrigo</Panel>
+    return (
+        <Panel>
+            <form className={styles.container}>
+                <Input label='Nome' />
+                <Button type="submit">Salvar dados</Button>
+            </form>
+        </Panel>
+
+    )
 }
