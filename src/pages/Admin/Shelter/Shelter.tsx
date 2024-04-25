2946acd1ccc860c668aec306a2477824c31af339
WellintonRafael <wellintonrafaelsantos@gmail.com>
2024-04-24 22:12:10 -0300

    finalizando formulário do abrigo

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -6,6 +6,8 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useHookFormMask } from 'use-mask-input';
+import { Toaster, toast } from 'sonner';
+import { updateShelter } from '../../../services/shelter/updateShelter';
 
 const shelterSchema = z.object({
     name: z
@@ -38,8 +40,28 @@ export function Shelter() {
 
     const registerWithMask = useHookFormMask(register);
 
-    function submit({ name }) {
-        console.log(name)
+    async function submit({ name, email, phone, whatsApp }: ShelterSchema) {
+        const toastId = toast.loading('Salvando dados')
+        try{
+            await updateShelter({
+                name,
+                email,
+                phone: phone.replace(/\D/g, ''),
+                whatsApp: whatsApp.replace(/\D/g, '')
+            })
+            toast.success('Dados salvos com sucesso', {
+                id: toastId,
+                closeButton: true
+            })
+        }
+        catch(error) {
+            toast.error('Não foi possível salvar os dados', {
+                id: toastId,
+                closeButton: true
+            })
+        }
+
+        console.log(name, email, phone, whatsApp)
     }
 
     return (
